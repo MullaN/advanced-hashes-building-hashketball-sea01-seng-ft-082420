94669700fcdeaed094a8b1e2bcae90454b8ec22d
Andrew Mullan <admullan@gmail.com>
2020-08-04 17:51:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,6 +6,17 @@ def game_hash
       colors: ["Black", "White"],
       players: [
         {
+          player_name: "Alan Anderson",
+          number: "0",
+          shoe: "16",
+          points: "22",
+          rebounds: "12",
+          assists: "12",
+          steals: "3",
+          blocks: "1",
+          slam_dunks: "1"
+        },
+        {
           player_name: "",
           number: "",
           shoe: "",
@@ -17,16 +28,37 @@ def game_hash
           slam_dunks: ""
         },
         {
-
+          player_name: "",
+          number: "",
+          shoe: "",
+          points: "",
+          rebounds: "",
+          assists: "",
+          steals: "",
+          blocks: "",
+          slam_dunks: ""
         },
         {
-
+          player_name: "",
+          number: "",
+          shoe: "",
+          points: "",
+          rebounds: "",
+          assists: "",
+          steals: "",
+          blocks: "",
+          slam_dunks: ""
         },
         {
-
-        },
-        {
-
+          player_name: "",
+          number: "",
+          shoe: "",
+          points: "",
+          rebounds: "",
+          assists: "",
+          steals: "",
+          blocks: "",
+          slam_dunks: ""
         }
       ]
     },
