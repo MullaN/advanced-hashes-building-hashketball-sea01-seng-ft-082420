1ce82166845a095987cb89fe68eb7bda2619b4ae
Andrew Mullan <admullan@gmail.com>
2020-08-04 17:47:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,14 @@
 def game_hash
   {
     home: {
-
+      team_name: "Brooklyn Nets",
+      colors: [],
+      players: []
     },
     away: {
-      
+      team_name: "Charlotte Hornets",
+      colors: [],
+      players: []
     }
   }
 end
