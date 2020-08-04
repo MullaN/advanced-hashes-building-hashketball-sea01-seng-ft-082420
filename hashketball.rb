f73da60b4a8e0f647dbf7e5c0321b8917388334f
Andrew Mullan <admullan@gmail.com>
2020-08-04 17:50:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,15 @@ def game_hash
       colors: ["Black", "White"],
       players: [
         {
-
+          player_name: "",
+          number: "",
+          shoe: "",
+          points: "",
+          rebounds: "",
+          assists: "",
+          steals: "",
+          blocks: "",
+          slam_dunks: ""
         },
         {
 
